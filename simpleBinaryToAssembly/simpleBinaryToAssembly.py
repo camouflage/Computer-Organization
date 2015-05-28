9b525d6eb20fbc9f78f7354a8e20fcb7013a12a8
camouflage <363419254@qq.com>
2015-05-28 15:00:16 +0800

    add register num

--- simpleBinaryToAssembly/simpleBinaryToAssembly.py
+++ simpleBinaryToAssembly/simpleBinaryToAssembly.py
@@ -7,13 +7,13 @@ for line in f:
 	# R-type
 	if line[0:6] == "000000":
 		if line[26:32] == "100000":
-			print "add"
+			print "add", int(line[16:21], 2), int(line[6:11], 2), int(line[11:16], 2)
 		elif line[26:32] == "100010":
-			print "sub"
+			print "sub", int(line[16:21], 2), int(line[6:11], 2), int(line[11:16], 2)
 		elif line[26:32] == "100100":
-			print "and"
+			print "and", int(line[16:21], 2), int(line[6:11], 2), int(line[11:16], 2)
 		elif line[26:32] == "100101":
-			print "or"
+			print "or", int(line[16:21], 2), int(line[6:11], 2), int(line[11:16], 2)
 		elif line[26:32] == "101010":
 			print "slt"
 		elif line[26:32] == "000000":
@@ -21,13 +21,13 @@ for line in f:
 		elif line[26:32] == "000110":
 			print "srlv"
 		elif line[26:32] == "011000":
-			print "mul"
+			print "mul", int(line[16:21], 2), int(line[6:11], 2), int(line[11:16], 2)
 		elif line[26:32] == "001000":
 			print "jr"
 
 	# I-type
 	elif line[0:6] == "001000":
-		print "addi"
+		print "addi", int(line[11:16], 2), int(line[6:11], 2)
 	elif line[0:6] == "001010":
 		print "slti"
 	elif line[0:6] == "001111":
@@ -37,17 +37,17 @@ for line in f:
 
 	# Branch
 	elif line[0:6] == "000100":
-		print "beq"
+		print "beq", int(line[6:11], 2), int(line[11:16], 2), int(line[16:32], 2)
 	elif line[0:6] == "000101":
-		print "bne"
+		print "bne", int(line[6:11], 2), int(line[11:16], 2), int(line[16:32], 2)
 	elif line[0:6] == "000111":
-		print "bgt"
+		print "bgt", int(line[6:11], 2), int(line[11:16], 2), int(line[16:32], 2)
 	elif line[0:6] == "000001":
-		print "bgez"
+		print "bgez", int(line[6:11], 2), "1", int(line[16:32], 2)
 
 	# lw & sw
 	elif line[0:6] == "100011":
-		print "lw"
+		print "lw", int(line[11:16], 2), str(int(line[16:32], 2)) + "(r" + str(int(line[6:11], 2)) + ")"
 	elif line[0:6] == "101011":
 		print "sw"
 
